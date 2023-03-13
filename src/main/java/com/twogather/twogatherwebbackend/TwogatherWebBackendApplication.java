55d29f4eabb1ec6460ae9b499103435f22682894
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2023-03-13 19:24:02 +0900

    Initial commit

---
+++ src/main/java/com/twogather/twogatherwebbackend/TwogatherWebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.twogather.twogatherwebbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwogatherWebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwogatherWebBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twogather/twogatherwebbackend/TwogatherWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twogather.twogatherwebbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwogatherWebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
