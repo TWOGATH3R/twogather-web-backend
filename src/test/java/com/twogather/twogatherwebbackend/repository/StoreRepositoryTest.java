9147889877e125a146ded441d5a657ead63dfa2d
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2023-05-18 17:07:32 +0900

    * refactoring -  isEqualsTo()는 원래 값을 비교하지만 객체의 경우 주솟값을 비교한다 -> 내용을 비교하도록 usingRecursiveComparison()사용 - 단위테스트에 @Transactional 적용

--- src/test/java/com/twogather/twogatherwebbackend/repository/StoreRepositoryTest.java
+++ src/test/java/com/twogather/twogatherwebbackend/repository/StoreRepositoryTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,8 +20,8 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
 @Import(QueryDslConfig.class)
+@SpringBootTest
 public class StoreRepositoryTest {
     @Autowired
     private StoreRepository storeRepository;
