b626e3717b4d2151b07eb728ce9267994cf1c9c9
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2023-05-19 19:00:22 +0900

    Update CorsConfig.java
    * bug
    - cors 변경: localhost:3000에 대해 허용

--- src/main/java/com/twogather/twogatherwebbackend/config/CorsConfig.java
+++ src/main/java/com/twogather/twogatherwebbackend/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true); //내서버가 응답을 할때 json을 자바스크립트에서 처리할 수 있게 할지
-        config.addAllowedOriginPattern("*"); //모든 아이피를 응답허용
+        config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedHeader("*"); //모든 header 응답허용
         config.addAllowedMethod("*"); //모든 post,get,put 허용
 
