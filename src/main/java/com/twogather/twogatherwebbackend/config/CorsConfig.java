535caee7707e9013c432c75a13519b87ff42029f
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2023-05-19 19:09:51 +0900

    Update CorsConfig.java
    * bug
    - list import문 추가

--- src/main/java/com/twogather/twogatherwebbackend/config/CorsConfig.java
+++ src/main/java/com/twogather/twogatherwebbackend/config/CorsConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
+import java.util.List;
 @Configuration
 public class CorsConfig {
 
