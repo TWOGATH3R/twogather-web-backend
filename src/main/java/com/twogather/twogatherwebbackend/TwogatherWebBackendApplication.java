a81a6e2bb3212e522952568c645b63d79a046d75
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2023-06-09 14:48:40 +0900

    오류 추적 위해 로깅레벨 변경

--- src/main/java/com/twogather/twogatherwebbackend/TwogatherWebBackendApplication.java
+++ src/main/java/com/twogather/twogatherwebbackend/TwogatherWebBackendApplication.java
@@ -2,8 +2,10 @@ package com.twogather.twogatherwebbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class TwogatherWebBackendApplication {
 
 	public static void main(String[] args) {
