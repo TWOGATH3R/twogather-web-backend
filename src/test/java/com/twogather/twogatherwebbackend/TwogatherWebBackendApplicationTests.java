14be2a74f44067acbc7f4620655ef2ef51f67778
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2023-06-10 00:15:24 +0900

    test로그파일분리

--- src/test/java/com/twogather/twogatherwebbackend/TwogatherWebBackendApplicationTests.java
+++ src/test/java/com/twogather/twogatherwebbackend/TwogatherWebBackendApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.util.ArrayList;
 import java.util.List;
