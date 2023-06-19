d6c639a29c35a3ee05ce5c31c9423a535bc9e4bd
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2023-06-19 21:54:44 +0900

    Update ImageAcceptanceTest.java

--- src/test/java/com/twogather/twogatherwebbackend/acceptance/ImageAcceptanceTest.java
+++ src/test/java/com/twogather/twogatherwebbackend/acceptance/ImageAcceptanceTest.java
@@ -236,7 +236,7 @@ public class ImageAcceptanceTest extends AcceptanceTest{
     private List<File> createNonImageFile(){
         List<File> fileList = new ArrayList<>();
 
-        File multipartFile = new File("src\\test\\resources\\files\\text.txt");
+        File multipartFile = new File(System.getProperty("user.dir") + "\\src\\test\\resources\\files\\text.txt");
 
         fileList.add(multipartFile);
         fileList.add(multipartFile);
