bfa276a7cb7ec61623845b9e9464a9968f170377
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2023-06-27 16:21:30 +0900

    twogather-web-backend-88 기능 보충 * feat - 이미지 최대 개수 초과하면 error throw - 자기가게엔 좋아요 못누르도록 추가 - 관리자 정보 수정, 조회

--- src/test/java/com/twogather/twogatherwebbackend/controller/StoreOwnerControllerTest.java
+++ src/test/java/com/twogather/twogatherwebbackend/controller/StoreOwnerControllerTest.java
@@ -3,6 +3,7 @@ package com.twogather.twogatherwebbackend.controller;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.twogather.twogatherwebbackend.dto.member.MemberResponse;
 import com.twogather.twogatherwebbackend.dto.member.MemberSaveRequest;
+import com.twogather.twogatherwebbackend.dto.member.MemberUpdateRequest;
 import com.twogather.twogatherwebbackend.dto.member.PasswordRequest;
 import com.twogather.twogatherwebbackend.service.MemberService;
 import com.twogather.twogatherwebbackend.service.StoreOwnerService;
@@ -102,7 +103,7 @@ public class StoreOwnerControllerTest extends ControllerTest{
     @DisplayName("가게주인정보업데이트")
     public void updateOwnerInfo_WhenUpdateOwnerInfo_ThenReturnOwnerInfo() throws Exception {
         //given
-        when(memberService.update(any())).thenReturn(
+        when(memberService.update(anyLong(), any())).thenReturn(
                 new MemberResponse(1l, "nick1", "dda@naver.com",
                 "가게주인이름"));
       //then
@@ -112,8 +113,8 @@ public class StoreOwnerControllerTest extends ControllerTest{
                         .content(
                                 objectMapper
                                         .registerModule(new JavaTimeModule())
-                                        .writeValueAsString(new MemberSaveRequest(
-                                                "ad@naer.com", "name1","Asdawd213", "홍길동"
+                                        .writeValueAsString(new MemberUpdateRequest(
+                                                "ad@naer.com", "name1", "홍길동"
                                         )))
                 )
                 .andExpect(status().isOk())
@@ -126,8 +127,7 @@ public class StoreOwnerControllerTest extends ControllerTest{
                         requestFields(
                                 fieldWithPath("email").type(JsonFieldType.STRING).description("이메일").optional(),
                                 fieldWithPath("username").type(JsonFieldType.STRING).description("로그인 ID").attributes(getUsernameFormat()).optional(),
-                                fieldWithPath("password").type(JsonFieldType.STRING).description("계정 비밀번호").attributes(getPasswordFormat()).optional(),
-                                fieldWithPath("name").type(JsonFieldType.STRING).description("사용자명(닉네임)").optional().attributes(getMemberNameFormat())
+                               fieldWithPath("name").type(JsonFieldType.STRING).description("사용자명(닉네임)").optional().attributes(getMemberNameFormat())
 
                         ),
                         responseFields(
