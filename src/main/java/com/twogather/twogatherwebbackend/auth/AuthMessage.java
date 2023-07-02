9956f29261b4371c63b4b74b6bafa93c3fa4fd98
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2023-07-02 17:58:44 +0900

    twogather-web-backend-88 기능 보충 * refactoring - 사용자 친화적 exception code 변경

--- src/main/java/com/twogather/twogatherwebbackend/auth/AuthMessage.java
+++ src/main/java/com/twogather/twogatherwebbackend/auth/AuthMessage.java
@@ -1,8 +1,8 @@
 package com.twogather.twogatherwebbackend.auth;
 
 public class AuthMessage {
-    public static final String INVALID_TOKEN = "토큰이 유효하지 않습니다";
-    public static final String EXPIRED_TOKEN = "토큰이 만료되었습니다";
+    public static final String INVALID_TOKEN = "잘못된 형식의 요청입니다";
+    public static final String EXPIRED_TOKEN = "세션이 만료되었습니다. 다시 로그인하십시오";
     public static final String NO_SUCH_MEMBER = "해당 회원이 존재하지 않습니다";
     public static final String INVALID_FORMAT = "잘못된 형식의 요청입니다";
 }
