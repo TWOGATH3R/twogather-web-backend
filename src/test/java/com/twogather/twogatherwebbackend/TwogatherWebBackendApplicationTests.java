9a86f5179d26190c9d6ed0055951b9e72cbddc63
J-I-H-O <jeongjiho0731@gmail.com>
2023-07-04 16:21:07 +0900

    twogather-web-backend-87 리뷰 및 대댓글
    * fix
    - Github Actions를 이용한 자동 배포가 실패하는 현상 수정

--- src/test/java/com/twogather/twogatherwebbackend/TwogatherWebBackendApplicationTests.java
+++ src/test/java/com/twogather/twogatherwebbackend/TwogatherWebBackendApplicationTests.java
@@ -4,9 +4,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootTest
 @TestPropertySource(properties = "spring.jpa.properties.hibernate.default_batch_fetch_size=10")
 class TwogatherWebBackendApplicationTests {
+
+	@PostConstruct
+	public void started() {
+		// timezone 세팅
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	@Test
 	void contextLoads() {
